95316d3d2617092c0ab731358c0b787612c62c65
jpasierbb <kubapasierb1@wp.pl>
2025-05-02 17:39:40 +0200

    chore: Added sending data in parts

--- client.py
+++ client.py
@@ -24,67 +24,70 @@ def dns_query(txid, domain):
 def send_dns_query(filepath):
     client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     client.settimeout(2)
-    
-    text = read_file(filepath)
-    if text is None:
+ 
+    text_full = read_file(filepath)
+    if text_full is None:
         print(f"Error with reading data from a file {filepath}")
         return
     
-    binary_list = text_to_binary_list(text)
-    binary_data = ''.join(binary for _, binary in binary_list)
+    text_parts = split_text_to_chunks(text_full)
 
-    domain = random.choice(DOMAINS)
+    for text in text_parts:
+        binary_list = text_to_binary_list(text)
+        binary_data = ''.join(binary for _, binary in binary_list)
 
-    #######################
-    #### Start danych #####
-    #######################
-    client.sendto(dns_query(SPECIAL_CHAR, domain).pack(), ('127.0.0.1', 5353))
-    print(f"Sent START TXID: {SPECIAL_CHAR}")
+        domain = random.choice(DOMAINS)
 
-    try:
-        response, _ = client.recvfrom(512)
-        print("Response received")
-    except socket.timeout:
-        print("No response received (timeout)")
+        #######################
+        #### Start danych #####
+        #######################
+        client.sendto(dns_query(SPECIAL_CHAR, domain).pack(), ('127.0.0.1', 5353))
+        print(f"Sent START TXID: {SPECIAL_CHAR}")
 
-    #######################
-    ##### Ukryte dane #####
-    #######################
-    for i in range(0, len(binary_data), 16):
-        selected_website = random.choice(DOMAINS)
-        chunk = binary_data[i:i+16]
-        if len(chunk) < 16:
-            chunk = chunk.ljust(16, '0')  # Dopelnij zerami do 16 bitów
-        
-        chunk = mix_two_chars_bits(chunk)
-        txid = int(chunk, 2) # Konwersja bitów na int
-
-        
-        client.sendto(dns_query(txid, selected_website).pack(), ('127.0.0.1', 5353))
-        print(f"Sent TXID: {txid} (chunk: {chunk})")
-
-        # Odbierz odpowiedz TTL i czekaj az minie TTL
         try:
             response, _ = client.recvfrom(512)
-            dns_response = DNSRecord.parse(response)
-            ttl = dns_response.rr[0].ttl if dns_response.rr else 1  # jeśli nie ma rr, domyśl TTL = 1s
-            print(f"Received response, sleeping for {ttl} seconds")
-            # time.sleep(ttl)
+            print("Response received")
         except socket.timeout:
-            print("No response received (timeout), sleeping for 1 second")
-            # time.sleep(1)
+            print("No response received (timeout)")
 
-    #######################
-    #### Koniec danych ####
-    #######################
-    client.sendto(dns_query(SPECIAL_CHAR, domain).pack(), ('127.0.0.1', 5353))
-    print(f"Sent END TXID: {SPECIAL_CHAR}")
+        #######################
+        ##### Ukryte dane #####
+        #######################
+        for i in range(0, len(binary_data), 16):
+            selected_website = random.choice(DOMAINS)
+            chunk = binary_data[i:i+16]
+            if len(chunk) < 16:
+                chunk = chunk.ljust(16, '0')  # Dopelnij zerami do 16 bitów
+            
+            chunk = mix_two_chars_bits(chunk)
+            txid = int(chunk, 2) # Konwersja bitów na int
 
-    try:
-        response, _ = client.recvfrom(512)
-        print("Response received")
-    except socket.timeout:
-        print("No response received (timeout)")
+            
+            client.sendto(dns_query(txid, selected_website).pack(), ('127.0.0.1', 5353))
+            print(f"Sent TXID: {txid} (chunk: {chunk})")
+
+            # Odbierz odpowiedz TTL i czekaj az minie TTL
+            try:
+                response, _ = client.recvfrom(512)
+                dns_response = DNSRecord.parse(response)
+                ttl = dns_response.rr[0].ttl if dns_response.rr else 1  # jeśli nie ma rr, domyśl TTL = 1s
+                print(f"Received response, sleeping for {ttl} seconds")
+                # time.sleep(ttl)
+            except socket.timeout:
+                print("No response received (timeout), sleeping for 1 second")
+                # time.sleep(1)
+
+        #######################
+        #### Koniec danych ####
+        #######################
+        client.sendto(dns_query(SPECIAL_CHAR, domain).pack(), ('127.0.0.1', 5353))
+        print(f"Sent END TXID: {SPECIAL_CHAR}")
+
+        try:
+            response, _ = client.recvfrom(512)
+            print("Response received")
+        except socket.timeout:
+            print("No response received (timeout)")
 
 
 if __name__ == "__main__":
